e72918de9f39c68b8f85a4bd5672c1383968f4f0
Islande Silva <islandefs@gmail.com>
2023-10-19 23:52:58 -0300

    chore: Add category page and category components

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,6 +1,24 @@
-const CatalogPage = () => {
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react"
+import CategoryItem from "./components/category-item";
+
+const CatalogPage = async () => {
+    const categories = await prismaClient.category.findMany({});
+
     return ( 
-        <h1>Catalog</h1>
+        <div className="p-5">
+            <Badge className="gap-1 text-base uppercase border-2 border-primary px-3 py-[0.375rem]" variant="outline">
+                <ShapesIcon size={16} />
+                Catálogo
+            </Badge>
+        
+
+        <div className="grid grid-cols-2 gap-8 pt-5 text-center justify-center">
+            {categories.map(category => <CategoryItem key={category.id} category={category} />)}
+        </div>
+        </div>
+
      );
 }
  
