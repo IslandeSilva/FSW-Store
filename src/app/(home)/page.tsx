a3be4bbc6198fca107649ba6c43a9a025ee45897
Islande Silva <islandefs@gmail.com>
2023-10-19 22:05:31 -0300

    feat: add second banner to homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -30,10 +30,20 @@ return (
 </div>
 
 <div className="mt-8">
+  <p className="font-bold uppercase pl-5 mb-3">Ofertas</p>
   <ProductList products={deals} />
 </div>
 
-
+<div className="mt-8">
+<Image 
+    src="/banner-mouses.png"
+    height={0}
+    width={0}
+    alt="Até 55% de desconto em mouses!"
+    className=" h-auto w-full p-5"
+    sizes="100vw"
+  />
+  </div>
 
 
 
