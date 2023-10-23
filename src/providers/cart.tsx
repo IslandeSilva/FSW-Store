3e00307fa89c0b4a8900192ea776edf90376dd7a
Islande Silva <islandefs@gmail.com>
2023-10-23 16:24:05 -0300

    feat: ensure products quantity can be modified on cart

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -14,6 +14,7 @@ interface ICartContext{
     cartBasePrise: number
     cartTotalDiscount: number
     addProductToCart: (product: CartProduct) => void;
+    decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -22,6 +23,7 @@ export const CartContext = createContext<ICartContext>({
     cartBasePrise: 0,
     cartTotalDiscount: 0,
     addProductToCart: () => {},
+    decreaseProductQuantity: () => {},
 });
 
 const CartProvider = ({children}: {children: ReactNode}) => {
@@ -53,10 +55,25 @@ const CartProvider = ({children}: {children: ReactNode}) => {
         setProducts((prev) => [...prev, product])
     };
 
+    const decreaseProductQuantity = (productId: string) => {
+
+    setProducts(prev => prev.map(cartProduct => {
+    if (cartProduct.id === productId) {
+        return {
+            ...cartProduct,
+            quantity: cartProduct.quantity -1
+        };
+    }
+    return cartProduct;
+    }).filter((cartProduct) => cartProduct.quantity > 0),
+  );
+ };
+
     return ( 
         <CartContext.Provider value={{
             products,
             addProductToCart,
+            decreaseProductQuantity,
             cartTotalPrice: 0,
             cartBasePrise: 0,
             cartTotalDiscount: 0,
